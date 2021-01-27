49d71547d68b7d3ad766031cd3faf21b26118381
Onsqa <45150212+Onsqa@users.noreply.github.com>
2021-01-27 10:20:31 +0200

    Pelaajat lisätty ja footeri vittuun :D

--- dc_jobs.lua
+++ dc_jobs.lua
@@ -1,16 +1,16 @@
--- Paskaa koodia Onsqalta
 ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 Citizen.CreateThread(function()
-while true do
-	local xPlayer  = ESX.GetPlayerFromId(source)
+	while true do
+		local xPlayer  = ESX.GetPlayerFromId(source)
 
-	local xPlayers = ESX.GetPlayers()
-	lanssi = 0
-	poliisi = 0
-	taksi = 0
-	meksu = 0
+		local xPlayers = ESX.GetPlayers()
+		lanssi = 0
+		poliisi = 0
+		taksi = 0
+		meksu = 0
+		pelaaja = 0
 
 		for i=1, #xPlayers, 1 do
 			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
@@ -26,28 +26,27 @@ while true do
 			if xPlayer.job.name == 'mechanic' then
 				meksu = meksu + 1
 			end
+			if xPlayer == xPlayer then
+				pelaaja = pelaaja + 1
 		end
-		laheta(poliisi,lanssi,taksi,meksu)
-		Wait(3600000) --Tunni välein nytten, aika on millisekunteina
+	end
+		laheta(poliisi,lanssi,taksi,meksu,pelaaja)
+		Wait(600000) -- Sopivasti
 	end
 end)
 
 
-
 function laheta(message)
-local DiscordWebHook ="webhookki"
+local DiscordWebHook ="Webbihuukki"
 local embeds = {
 	{
 		["title"]="Työntekijät",
 		["type"]="rich",
 		["color"] = 2061822,
-		["description"] = "Ensihoitajia: " .. lanssi .. "\nPoliiseja: " .. poliisi .. "\nTakseja: " .. taksi .. "\nMekaanikkoja: " .. meksu,
-		["footer"]=  {
-			["text"] =  "Logit " ..os.date("%d.%m. klo %X"),
-		},
+		["description"] = "Pelaajia: " .. pelaaja .. "\nEnsihoitajia: " .. lanssi .. "\nPoliiseja: " .. poliisi .. "\nTakseja: " .. taksi .. "\nMekaanikkoja: " .. meksu,
 	}
 }
 
-	if message == nil or message == '' then return FALSE end  -- Tuossa missä lukee työnvalvoja niin se on se nimi joka näkyy discordis kannul
+	if message == nil or message == '' then return FALSE end
 	PerformHttpRequest(DiscordWebHook, function(err, text, headers) end, 'POST', json.encode({ username = "Työnvalvoja", embeds = embeds}), { ['Content-Type'] = 'application/json' })
 end
